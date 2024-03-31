dc6e30fabed231c6230b078aa39e8396ac500040
RahulKDubey <rahuld0902@gmail.com>
2024-03-31 22:22:34 +0530

    now using polygon mumbai testnet

--- Context/contants.js
+++ Context/contants.js
@@ -1,8 +1,8 @@
-import crowdFunding from "./CrowdFunding.json";
+import CrowdFunding from "./CrowdFunding.json";
 
 //NFT MARKETPLACE
 //LOCAL HOST TESTING HARDHAT
 // export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // POLYGON MUMBAI
-export const CrowdFundingAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-export const CrowdFundingABI = crowdFunding.abi;
+export const CrowdFundingAddress = "0xB0bcb7140bD83E181fD7D661a369d4EcAf1B9B70";
+export const CrowdFundingABI = CrowdFunding.abi;
