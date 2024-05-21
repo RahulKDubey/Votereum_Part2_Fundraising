52d2f8974704a72620ab2fff50c1df5e326d9095
RahulKDubey <rahuld0902@gmail.com>
2024-05-21 23:18:18 +0530

    address

--- Context/contants.js
+++ Context/contants.js
@@ -4,5 +4,5 @@ import CrowdFunding from "./CrowdFunding.json";
 //LOCAL HOST TESTING HARDHAT
 // export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // POLYGON MUMBAI
-export const CrowdFundingAddress = "0xdAAf9df168d28ef07f28CF41f4db6091b4538AC4";
+export const CrowdFundingAddress = "0xc8Ad76dd8E3c0a389cA0076cCdFA19235db9D023";
 export const CrowdFundingABI = CrowdFunding.abi;
